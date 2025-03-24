42f47a53ad665d50efe86a101f7e8fc804688463
Tim <lovezww2011@gmail.com>
2025-03-24 22:19:26 +0800

    add shadcn

--- web/src/app/(admin)/admin/main/page.jsx
+++ web/src/app/(admin)/admin/main/page.jsx
@@ -4,6 +4,8 @@ import OverViewInfo from "@/components/admin/main/overViewInfo";
 import VisitInfo from "@/components/admin/main/visitInfo";
 import SysInfo from "@/components/admin/main/sysInfo";
 import {useRouter} from "next/navigation";
+import {Button} from "@/components/ui/button";
+import { MailOpen } from "lucide-react"
 
 export default function Page() {
     const router = useRouter();
@@ -13,6 +15,9 @@ export default function Page() {
         <>
             <div className="bg-gray-100 px-4 py-4">
                 <div className="flex flex-col gap-4">
+                    <Button>
+                        <MailOpen /> Login with Email
+                    </Button>
                     <OverViewInfo/>
                     <VisitInfo/>
                     <SysInfo/>
